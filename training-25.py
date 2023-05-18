3dbd25032f613e06a3c24753eba9d93c2637c617
leesemin <leesemin222@gmail.com>
2023-05-19 00:24:41 +0900

    파이썬 적용 연습중

---
+++ training-25.py
@@ -0,0 +1,9 @@
+dummyText : str = '아버지가방에들어가신다'
+reFineNounData : list[str] = ['아버지','방']
+spacing : str = ' '
+
+
+def spacingLetter(text:str, nounData : list[str], spacing : str) -> str:
+    result = ""
+
+    return result
